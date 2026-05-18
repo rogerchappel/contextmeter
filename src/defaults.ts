8df571b53a76453000f486303f802dfc0f33e392
Roger Chappel <miscanalysis@gmail.com>
2026-05-19 06:34:53 +1000

    feat: add default scan patterns

---
+++ src/defaults.ts
@@ -0,0 +1,26 @@
+export const DEFAULT_INCLUDE = ["**/*"];
+
+export const DEFAULT_EXCLUDE = [
+  "**/.git/**",
+  "**/node_modules/**",
+  "**/dist/**",
+  "**/build/**",
+  "**/coverage/**",
+  "**/.next/**",
+  "**/.turbo/**",
+  "**/.cache/**",
+  "**/*.png",
+  "**/*.jpg",
+  "**/*.jpeg",
+  "**/*.gif",
+  "**/*.webp",
+  "**/*.ico",
+  "**/*.pdf",
+  "**/*.zip",
+  "**/*.tar",
+  "**/*.gz"
+];
+
+export const DEFAULT_CATEGORY = "uncategorized";
+
+export const DEFAULT_CONFIG_FILE = "contextmeter.json";
